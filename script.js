6f29f205508b4c58e12e0095e21b328ebc95babd
Aleksandr Elovskih <aleksandr@MacBook-Air-Aleksandr.local>
2024-06-23 22:00:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let gmiyk = document.querySelector('.rr');
+gmiyk.addEventListener('click', function aler() {
+    alert('Кнопка нажата!')
+} );
+
+let exeBB= document.querySelector('.exeBB');
+
+let examRight = document.getElementById('exam1');
+exeBB.addEventListener('click', function aler2 (){
+    if (examRight.checked) {
+        alert('Ответ верный!')
+    } else {
+        alert('Ответ неверный!')
+    }
+    //alert('работает')
+});
